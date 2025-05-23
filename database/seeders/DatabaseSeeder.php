f6975a0f9e9b7f86d1f8a1b55dac6fdb0fedcb9c
vitalii <bondarfamily123@gmail.com>
2025-05-23 18:02:28 +0300

    feat: expand structure of db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             WarehouseSeeder::class,
             ProductSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
